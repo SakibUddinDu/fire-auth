435c9655b9125155d36c3a929381fdb200e64f81
Mainuddinhasansakib <sakibuddin831@gmail.com>
2021-03-15 08:57:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import React, { useState } from 'react';
 import './App.css';
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+ const [user, setUser] = useState({
+    isSignedIn : false,
+    name : ' ',
+    email : ' ',
+    photo : ' '
+ })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn= () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res =>{
+      const {displayName, email, photoURL} = res.user;
+      // console.log(displayName, email, photoURL)
+      // console.log(res);
+      const signedInUser = {
+        isSignedIn : true,
+        name : displayName,
+        email : email,
+        photo : photoURL
+     }
+     setUser(signedInUser)
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      // var errorCode = error.code;
+    console.log(error) 
+    console.log(error.message) 
+      // // The email of the user's account used.
+      // var email = error.email;
+      // // The firebase.auth.AuthCredential type that was used.
+      // var credential = error.credential;
+      // // ...
+    });
+  }
+  const handleSignOut = () =>{
+    firebase.auth().signOut()
+    .then(res=> {
+      const signedOutUser = {
+        isSignedIn : false,
+        name : ' ',
+        email : ' ',
+        photo : ' '
+     }
+     setUser(signedOutUser)
+    })
+      // Sign-out successful.
+    .catch(error => {
+      // An error happened.
+    });
+    // console.log('SignOut Clicked');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ?  <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleSignIn}>Sign In</button>
+      }
+      
+    {
+      user.isSignedIn && <div>
+         <p>Welcome, {user.name}</p>
+         <p>Email : {user.email}</p>
+         <img src={user.photo} alt=""/>
+      </div>
+    }
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwN7zoVrYIe6N-v3RVETqtWWkeGsoA5tk",
+    authDomain: "ammo-jan-simple.firebaseapp.com",
+    projectId: "ammo-jan-simple",
+    storageBucket: "ammo-jan-simple.appspot.com",
+    messagingSenderId: "666202949488",
+    appId: "1:666202949488:web:a2b563ac25b73a2852f1b5"
+  };
+
+  export default firebaseConfig;
